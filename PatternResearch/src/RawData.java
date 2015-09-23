55a3b8a1c6fec95d7ea80fa26334956eba3e3140
mtracy01 <matthetracy@gmail.com>
2015-09-22 20:28:02 -0400

    Replaced with Abstract class

--- PatternResearch/src/RawData.java
+++ PatternResearch/src/RawData.java
@@ -1,4 +1,24 @@
+import java.util.*;
 
 public class RawData {
-
+	public ArrayList<ArrayList<double[]>> data = new ArrayList<>();
+	public String dataName;
+	public int count;
+	public double durations;
+	
+	public RawData(String dataName, int count, double durations){
+		this.dataName = dataName;
+		this.count = count;
+		this.durations = durations;
+	}
+	
+	public void addRecordedPattern(ArrayList<double[]>pattern){
+		data.add(pattern);
+	}
+	
+	public ArrayList<double[]> getRawPattern(int index){
+		if(index<count)
+			return data.get(index);
+		return null;
+	}
 }
