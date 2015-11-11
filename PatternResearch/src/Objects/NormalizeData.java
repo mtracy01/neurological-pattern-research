a763568ba7079856c84bb6f2c90c8b9499ffcb3c
mtracy01 <matthetracy@gmail.com>
2015-11-11 18:58:59 -0500

    some minor refactoring and cleaning

--- PatternResearch/src/Objects/NormalizeData.java
+++ PatternResearch/src/Objects/NormalizeData.java
@@ -3,12 +3,12 @@ package Objects;
 import java.util.ArrayList;
 
 public class NormalizeData {
-	public static Tuple<Double[], Double[]> findMax(ArrayList<double[]> parsedData){
+	public static Tuple<Double[], Double[]> findMaxMin(ArrayList<double[]> parsedData){
 		Tuple<Double[],Double[]> maxMin = new Tuple(new Double[14], new Double[14]);
 		/*x stores all max values, y stores all min values*/
 		for(int i = 0 ; i< 14; i++){
-			maxMin.x[i] = 0.0;
-			maxMin.y[i] = (double)Integer.MAX_VALUE;
+			maxMin.x[i] = Double.MIN_VALUE;
+			maxMin.y[i] = Double.MAX_VALUE;
 		}
 		int maxLen = 14;
 		if(parsedData!= null ){
@@ -16,16 +16,13 @@ public class NormalizeData {
 			int length = parsedData.size();
 			while(i < length){
 				int j = 0;
-
 				while( j < maxLen){
-					
+
 					double[] arr = parsedData.get(i);
-					if(maxMin.x[j] < arr[j]){
+					if(maxMin.x[j] < arr[j])
 						maxMin.x[j] = arr[j];
-					}
-					if(maxMin.y[j] > arr[j]){
+					if(maxMin.y[j] > arr[j])
 						maxMin.y[j] = arr[j];
-					}
 					j++;
 				}
 				i++;
@@ -35,19 +32,17 @@ public class NormalizeData {
 	}
 	
 	public static ParsedData normalizeParsedData(ParsedData pd){
-		Tuple<Double[],Double[]> maxMin = findMax(pd.getDataPoints());
+		Tuple<Double[],Double[]> maxMin = findMaxMin(pd.getDataPoints());
 		ParsedData pdnew = new ParsedData(pd.getTitle(),
 					pd.getRecordingDuration(),pd. getSampleRate(),pd.getSubject());
 		ArrayList<double[]> temp = pd.getDataPoints();
-		int k = 0;
 		for(double[] arr: temp){
 			/* Normalize by newVal = (currVal-min)/(max-min)*/
+			int k = 0;
 			while(k < 14){
-				arr[k] = (arr[k]- maxMin.y[k])/(maxMin.x[k] - maxMin.y[k]);
-				//System.out.println("Here   "+arr[k]);
+				arr[k] = (arr[k]- maxMin.y[k])/(maxMin.x[k] - maxMin.y[k]);;
 				k++;
 			}
-			k = 0;
 			pdnew.addDataPoint(arr);
 		}
 		return pdnew;
